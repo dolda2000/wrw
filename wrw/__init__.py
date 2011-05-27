d997912890e3b41898eecbae93c87ec2927c826c
Fredrik Tolf <fredrik@dolda2000.com>
2011-05-27 08:14:32 +0200

    Added a per-session class-based handler wrapper.

--- wrw/__init__.py
+++ wrw/__init__.py
@@ -1,7 +1,7 @@
 __all__ = ["request", "wsgiwrap", "restart", "cookie", "formdata"]
 
 from req import request
-from util import wsgiwrap
+from util import wsgiwrap, persession
 from dispatch import restart
 import cookie
 from form import formdata
--- wrw/util.py
+++ wrw/util.py
@@ -1,6 +1,21 @@
-import req, dispatch
+import req, dispatch, session
 
 def wsgiwrap(callable):
     def wrapper(env, startreq):
         return dispatch.handle(req.request(env), startreq, callable)
     return wrapper
+
+def persession(data = None):
+    def dec(callable):
+        def wrapper(req):
+            sess = session.get(req)
+            if callable not in sess:
+                if data is None:
+                    sess[callable] = callable()
+                else:
+                    if data not in sess:
+                        sess[data] = data()
+                    sess[callable] = callable(data)
+            return sess[callable].handle(req)
+        return wrapper
+    return dec

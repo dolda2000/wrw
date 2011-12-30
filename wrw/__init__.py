bbdebbab6c98d8611bc8faa197df226d29e4cb2b
Fredrik Tolf <fredrik@dolda2000.com>
2011-12-30 02:33:09 +0100

    Added a mod_python-style form-data application decorator.

--- wrw/__init__.py
+++ wrw/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ["wsgiwrap", "restart", "cookie", "formdata"]
 
-from util import wsgiwrap, persession, sessiondata
+from util import wsgiwrap, formparams, persession, sessiondata, autodirty, manudirty
 from dispatch import restart
 import cookie
 from form import formdata

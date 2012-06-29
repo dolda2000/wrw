be8dc8230fa1c6eb5104fa4983d23e585a40492b
Fredrik Tolf <fredrik@dolda2000.com>
2012-06-29 04:34:43 +0200

    Import specdirty into __init__.

--- wrw/__init__.py
+++ wrw/__init__.py
@@ -1,7 +1,7 @@
 __all__ = ["wsgiwrap", "restart", "cookie", "formdata"]
 
 import proto
-from util import wsgiwrap, formparams, persession, sessiondata, autodirty, manudirty
+from util import wsgiwrap, formparams, persession, sessiondata, autodirty, manudirty, specdirty
 from dispatch import restart
 import cookie
 from form import formdata

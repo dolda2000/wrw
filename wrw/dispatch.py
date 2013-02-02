77dd732a3b3b604a6029e748ffd4fb0b9760642d
Fredrik Tolf <fredrik@dolda2000.com>
2013-02-02 06:25:37 +0100

    Moved the iterproxy to an optionally usable preiter in wrw.util.

--- wrw/dispatch.py
+++ wrw/dispatch.py
@@ -16,32 +16,6 @@ def mangle(result):
         return result
     return [str(result)]
 
-class iterproxy(object):
-    # Makes sure iter(real).next() is called immediately, in order to
-    # let generator code run.
-    def __init__(self, real):
-        self.bk = real
-        self.bki = iter(real)
-        self._next = [None]
-        self.next()
-
-    def __iter__(self):
-        return self
-
-    def next(self):
-        if self._next is None:
-            raise StopIteration()
-        ret = self._next[0]
-        try:
-            self._next[:] = [self.bki.next()]
-        except StopIteration:
-            self._next = None
-        return ret
-
-    def close(self):
-        if hasattr(self.bk, "close"):
-            self.bk.close()
-
 def defaulterror(req, excinfo):
     import resp
     traceback.print_exception(*excinfo)
@@ -60,7 +34,7 @@ def handle(req, startreq, handler):
         resp = [""]
         while True:
             try:
-                resp = iterproxy(handler(req))
+                resp = handler(req)
                 break
             except restart, i:
                 handler = i.handle
